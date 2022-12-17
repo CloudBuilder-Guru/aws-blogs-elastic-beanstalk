88a333810aba12335c6ec56f107cc0d89ffbc4aa
Vikas Solegaonkar <vikas.solegaonkar@gmail.com>
2022-12-17 18:13:16 +0530

    Add files via upload

---
+++ 01/app.js
@@ -0,0 +1,17 @@
+const http = require("http");
+const fs = require("fs");
+const html = fs.readFileSync("index.html");
+
+const port = process.env.PORT || 3000;
+
+const server = http.createServer(function (req, res) {
+  res.writeHead(200);
+  res.write(html);
+  res.end();
+});
+
+// Listen on port 3000, IP defaults to 127.0.0.1
+server.listen(port);
+
+// Put a friendly message on the terminal
+console.log("Server running at http://127.0.0.1:" + port + "/");
